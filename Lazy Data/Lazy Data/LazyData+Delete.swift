fb9ffcface145320d68759f296e9406d66824320
Marcos <marcosrodriguez@me.com>
2016-03-13 15:12:22 -0400

    Basics of Core Data

---
+++ Lazy Data/Lazy Data/LazyData+Delete.swift
@@ -0,0 +1,19 @@
+//
+//  LazyData+Delete.swift
+//  Lazy Data
+//
+//  Created by Marcos Rodriguez on 3/13/16.
+//  Copyright © 2016 Marcos Rodriguez Vélez. All rights reserved.
+//
+
+import Foundation
+
+extension LazyData {
+    
+    
+    // MARK: - Delete all objects in Managed Context.
+    class func deleteAllObjects() {
+        
+    }
+    
+}
--- Lazy Data/Lazy Data/LazyData.swift
+++ Lazy Data/Lazy Data/LazyData.swift
@@ -13,23 +13,82 @@ class LazyData {
     
     // MARK: - Singleton
     /*This is the instance, of LazyData, that will be used all the time. */
-    private static let instance: LazyData = LazyData()
-    private var managedObjectContext: NSManagedObjectContext = NSManagedObjectContext(concurrencyType: .PrivateQueueConcurrencyType)
+    private static let sharedInstance = LazyData()
     
+    // MARK: - Properties
+    private var managedObjectModel: NSManagedObjectModel = NSManagedObjectModel()
+    var dataModelName: String? = "" {
+        didSet {
+            let modelURL = NSBundle.mainBundle().URLForResource(dataModelName, withExtension: "momd")!
+            self.managedObjectModel = NSManagedObjectModel(contentsOfURL: modelURL)!
+        }
+    }
     
-    // MARK: - Start the Managed Context, and other needed objects.
-    class func start() {
-        
+    // MARK: - Initializer
+    convenience init(dataModelName: String) {
+        self.init()
+        self.dataModelName = dataModelName
     }
     
     // MARK: - Reset the Managed Context.
     class func reset() {
-        
+
     }
+        
+    // MARK: - Core Data Boilerplate Code
+    private lazy var applicationDocumentsDirectory: NSURL = {
+        // The directory the application uses to store the Core Data store file. This code uses a directory named "com.marcosrdz.lazydata" in the application's documents Application Support directory.
+        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        return urls[urls.count-1]
+    }()
     
-    // MARK: - Delete all objects in Managed Context.
-    class func deleteAllObjects() {
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
+        // Create the coordinator and store
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("SingleViewCoreData.sqlite")
+        var failureReason = "There was an error creating or loading the application's saved data."
+        do {
+            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
+        } catch {
+            // Report any error we got.
+            var dict = [String: AnyObject]()
+            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
+            dict[NSLocalizedFailureReasonErrorKey] = failureReason
+            
+            dict[NSUnderlyingErrorKey] = error as NSError
+            let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+            // Replace this with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
+            abort()
+        }
         
+        return coordinator
+    }()
+    
+    private lazy var managedObjectContext: NSManagedObjectContext = {
+        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
+        let coordinator = self.persistentStoreCoordinator
+        var managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = coordinator
+        return managedObjectContext
+    }()
+    
+    // MARK: - Core Data Saving support
+    func saveContext () {
+        if managedObjectContext.hasChanges {
+            do {
+                try managedObjectContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+                abort()
+            }
+        }
     }
+
     
 }
